e91d64692801136b208fd2af116cc1d654457448
retromodernism <43184201+retromodernism@users.noreply.github.com>
2021-07-15 18:51:11 +0800

    Добавил Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import s from "./App.module.scss";
 import Header from "./components/header";
+import Home from "./components/home";
 
 const App = (props) => {
   return (
     <div className={s.app}>
       <Header />
+      <main className={s.main}>
+        <Home />
+      </main>
     </div>
   );
 };
---
+++ src/components/home/index.jsx
@@ -0,0 +1,103 @@
+import s from "./index.module.scss";
+import Typical from "react-typical";
+import ReactTypingEffect from "react-typing-effect";
+import "react-modal-video/scss/modal-video.scss";
+import ModalVideo from "react-modal-video";
+import { useState } from "react";
+import classname from "classnames";
+import { ReactComponent as Loupe } from "./src/loupe.svg";
+import { ReactComponent as Format } from "./src/format.svg";
+
+const Home = (props) => {
+  const [videoIsOpen, setVideoOpen] = useState(false);
+  const [buttonIsHover, setButtonIsHover] = useState(false);
+
+  return (
+    <section className={s.home}>
+      <div className={s._bg0}></div>
+      <div className={s._bg1}></div>
+      <div className={s._bg2}></div>
+      <div className={s._bg3}></div>
+      <div className={s._bg4}></div>
+      <div className={s._bg5}></div>
+      <div className={s._bg6}></div>
+      <div className={s._bg}></div>
+      <div className={s._content}>
+        <div className={s._title}>
+          Винное
+          <br />
+          фан-казино
+        </div>
+        <div className={s._subtitle}>
+          * все законно, мы не используем настоящие деньги
+        </div>
+        <div className={s._typingText}>
+          {/* <Typical
+            steps={[
+              "для вечеринок",
+              3000,
+              "на день рождения",
+              3000,
+              "на свадьбу",
+              3000,
+              "на корпоратив",
+              3000,
+              "для частного мероприятия",
+              3000,
+            ]}
+            loop={Infinity}
+            wrapper="p"
+          /> */}
+          <ReactTypingEffect
+            typingDelay={0}
+            speed={100}
+            eraseSpeed={100}
+            eraseDelay={3000}
+            text={[
+              "для вечеринок",
+              "на день рождения",
+              "на свадьбу",
+              "на корпоратив",
+              "для частного мероприятия",
+            ]}
+          />
+        </div>
+        <button
+          className={classname(s._videoButton, {
+            [s._videoButton_hover]: buttonIsHover,
+          })}
+          onClick={setVideoOpen.bind(null, true)}
+          onMouseEnter={setButtonIsHover.bind(null, true)}
+          onMouseLeave={setButtonIsHover.bind(null, false)}
+        >
+          <p>Смотреть видео</p>
+          <div className={s._videoButtonIcon}></div>
+        </button>
+        <div className={s._features}>
+          <div className={s._feature}>
+            <div className={s._featureNumber}>4</div>
+            <p className={s._featureTitle}>сорта вина</p>
+          </div>
+          <div className={s._feature}>
+            <Loupe style={{ marginTop: "7px" }} />
+            <p className={s._featureTitle}>интересные факты</p>
+          </div>
+          <div className={s._feature}>
+            <Format />
+            <p className={s._featureTitle}>новый формат</p>
+          </div>
+        </div>
+      </div>
+
+      <ModalVideo
+        channel="youtube"
+        autoplay={1}
+        isOpen={videoIsOpen}
+        videoId={"fZd3IMBfMB0"}
+        onClose={setVideoOpen.bind(null, false)}
+      />
+    </section>
+  );
+};
+
+export default Home;
