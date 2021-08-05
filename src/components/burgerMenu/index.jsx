b2694231b90bef33a29f322bf9f931504f281231
retromodernism <43184201+retromodernism@users.noreply.github.com>
2021-08-05 16:29:28 +0800

    Добавил ссылки в бургер

--- src/components/burgerMenu/index.jsx
+++ src/components/burgerMenu/index.jsx
@@ -5,8 +5,17 @@ import cx from "classnames";
 import { ReactComponent as XIcon } from "./src/xIcon.svg";
 import { closeBurger } from "../../redux/modules/burger";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const BurgerMenu = ({ closeBurger, ...props }) => {
+const BurgerMenu = ({ casinos, closeBurger, ...props }) => {
+  const foodCasinos = casinos
+    .filter(({ type }) => type === "foodCasino")
+    .map(({ title, url }) => ({ title, url }));
+
+  const classicCasinos = casinos
+    .filter(({ type }) => type === "classicCasino")
+    .map(({ title, url }) => ({ title, url }));
+
   const [foodCasinosIsExpanded, setFoodCasinosIsExpanded] = useState(true);
   const [classicCasinosIsExpanded, setClassicCasinosIsExpanded] =
     useState(false);
@@ -34,54 +43,11 @@ const BurgerMenu = ({ closeBurger, ...props }) => {
             <p>Кулинарное казино</p>
             {foodCasinosIsExpanded && (
               <div className={s._itemCasinos}>
-                <a href="/" className={s._casino}>
-                  Безалкогольное
-                </a>
-                <a href="/" className={s._casino}>
-                  Винное
-                </a>
-                <a href="/" className={s._casino}>
-                  Шампанское
-                </a>
-                <a href="/" className={s._casino}>
-                  Виски
-                </a>
-                <a href="/" className={s._casino}>
-                  Настойки
-                </a>
-                <a href="/" className={s._casino}>
-                  Чайное
-                </a>
-                <a href="/" className={s._casino}>
-                  Кофейное
-                </a>
-                <a href="/" className={s._casino}>
-                  Молекулярное
-                </a>
-                <a href="/" className={s._casino}>
-                  Мясное
-                </a>
-                <a href="/" className={s._casino}>
-                  Сырное
-                </a>
-                <a href="/" className={s._casino}>
-                  Сало
-                </a>
-                <a href="/" className={s._casino}>
-                  Хлебное
-                </a>
-                <a href="/" className={s._casino}>
-                  Чили
-                </a>
-                <a href="/" className={s._casino}>
-                  Шоколадное
-                </a>
-                <a href="/" className={s._casino}>
-                  Медовое
-                </a>
-                <a href="/" className={s._casino}>
-                  Пивное
-                </a>
+                {foodCasinos.map(({ title, url }) => (
+                  <Link to={url} className={s._casino} onClick={closeBurger}>
+                    {title}
+                  </Link>
+                ))}
               </div>
             )}
           </li>
@@ -101,33 +67,11 @@ const BurgerMenu = ({ closeBurger, ...props }) => {
             <p>Классическое казино</p>
             {classicCasinosIsExpanded && (
               <div className={s._itemCasinos}>
-                <a href="/" className={s._casino}>
-                  Рулетка
-                </a>
-                <a href="/" className={s._casino}>
-                  Покер
-                </a>
-                <a href="/" className={s._casino}>
-                  Блэк Джэк
-                </a>
-                <a href="/" className={s._casino}>
-                  Аттракционы
-                </a>
-                <a href="/" className={s._casino}>
-                  Колесо фортуны
-                </a>
-                <a href="/" className={s._casino}>
-                  Бильярд
-                </a>
-                <a href="/" className={s._casino}>
-                  Онлайн казино
-                </a>
-                <a href="/" className={s._casino}>
-                  Игровые автоматы
-                </a>
-                <a href="/" className={s._casino}>
-                  Крэпс
-                </a>
+                {classicCasinos.map(({ title, url }) => (
+                  <Link to={url} className={s._casino} onClick={closeBurger}>
+                    {title}
+                  </Link>
+                ))}
               </div>
             )}
           </li>
@@ -143,4 +87,9 @@ const BurgerMenu = ({ closeBurger, ...props }) => {
   );
 };
 
-export default connect(null, { closeBurger })(BurgerMenu);
+export default connect(
+  (state) => ({
+    casinos: state.casinos.casinos,
+  }),
+  { closeBurger }
+)(BurgerMenu);
